b0905ab72dab48f6c1c98465393c9c2f7c5694b0
Sam Verschueren <sam.verschueren@gmail.com>
2015-11-01 12:48:09 +0100

    Use getStream

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import {spawn} from 'child_process';
 import test from 'ava';
 import pathExists from 'path-exists';
+import getStream from 'get-stream';
 import {version as pkgVersion} from '../package.json';
 
 process.chdir(__dirname);
@@ -10,7 +11,7 @@ test.serial('generate screenshot', t => {
 	const cp = spawn('../cli.js', ['yeoman.io', '320x240']);
 
 	cp.on('close', () => {
-		t.assert(pathExists.sync('yeoman.io-320x240.png'));
+		t.true(pathExists.sync('yeoman.io-320x240.png'));
 		fs.unlinkSync('yeoman.io-320x240.png');
 
 		t.end();
@@ -29,13 +30,10 @@ test('remove temporary files on cancel', t => {
 	}, 500);
 });
 
-test('show error if no url is specified', t => {
-	t.plan(1);
+test('show error if no url is specified', async t => {
+	const data = await getStream(spawn('../cli.js', ['320x240']).stderr);
 
-	const cp = spawn('../cli.js', ['320x240']);
-
-	cp.stderr.setEncoding('utf8');
-	cp.stderr.on('data', data => t.regexTest(/Specify a url/, data));
+	t.regexTest(/Specify a url/, data);
 });
 
 test('use 1366x768 as default resolution', t => {
@@ -60,20 +58,14 @@ test('generate screenshots using keywords', t => {
 	});
 });
 
-test('show help screen', t => {
-	t.plan(1);
+test('show help screen', async t => {
+	const data = await getStream(spawn('../cli.js', ['--help']).stdout);
 
-	const cp = spawn('../cli.js', ['--help']);
-
-	cp.stdout.setEncoding('utf8');
-	cp.stdout.on('data', data => t.regexTest(/Capture screenshots of websites in various resolutions./, data));
+	t.regexTest(/Capture screenshots of websites in various resolutions./, data);
 });
 
-test('show version', t => {
-	t.plan(1);
+test('show version', async t => {
+	const data = await getStream(spawn('../cli.js', ['--version']).stdout);
 
-	const cp = spawn('../cli.js', ['--version']);
-
-	cp.stdout.setEncoding('utf8');
-	cp.stdout.on('data', data => t.regexTest(new RegExp(pkgVersion), data));
+	t.regexTest(new RegExp(pkgVersion), data)
 });
